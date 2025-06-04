e34ba197da2d1b741e57a2dd13da242734fc2fae
waj310 <waj310@yahoo.com>
2025-06-04 12:35:11 -0400

    added files

--- src/app.py
+++ src/app.py
@@ -38,4 +38,4 @@ def addnumber (number1,number2):
  return str(sum)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
